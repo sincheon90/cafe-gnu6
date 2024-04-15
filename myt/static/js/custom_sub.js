bfee21a193bc187ff34bf89f268677376447ef04
sincheon <sinchon90@gmail.com>
2024-04-15 12:18:38 +0900

    헤더 수정 완료

--- myt/static/js/custom_sub.js
+++ myt/static/js/custom_sub.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var navbar = document.querySelector("nav");
+  var navbars = document.querySelectorAll("nav");
   var navbarSquare = document.querySelector(".navsquare"); // 상단바 네모
   var navbarText = document.querySelectorAll(".navbutton"); // 상단텍스트
-  var navbarSquraeSub = document.querySelector(".navsquare-sub") // 서브메뉴 네모
+  var navbarSquareSubs = document.querySelectorAll(".navsquare-sub") // 서브메뉴 네모
   var navbarTextSub = document.querySelectorAll(".navsubbutton") // 서브텍스트
 
   init();
@@ -12,18 +12,18 @@ document.addEventListener("DOMContentLoaded", function () {
     navDownColorChange();
   }
 
-  navbar.addEventListener("mouseover", function () {
-    if (window.matchMedia("(min-width: 992px)").matches) {
-      mouseOverNav();
-    }
-
-  })
-
-  navbar.addEventListener("mouseout", function () {
-    if (window.matchMedia("(min-width: 992px)").matches) {
-      mouseOutNav();
-    }
-  })
+  navbars.forEach(function(navbar){
+    navbar.addEventListener("mouseover", function () {
+      if (window.matchMedia("(min-width: 992px)").matches) {
+        mouseOverNav();
+      }
+    });
+    navbar.addEventListener("mouseout", function () {
+      if (window.matchMedia("(min-width: 992px)").matches) {
+        mouseOutNav();
+      }
+    });
+  });
 
   // Applying mouseover effects
   setupMouseEvents(navbarText, "mouseover", "text-over", "text-menu");
@@ -87,16 +87,19 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   function subNavUp() {
     if (window.matchMedia("(min-width: 992px)").matches) {
-      navbarSquraeSub.classList.remove("expanded");
-      navbarSquraeSub.classList.add("shrink");
+      navbarSquareSubs.forEach(function(element) {
+        element.classList.remove("expanded");
+        element.classList.add("shrink");
+      });
     }
   }
   function subNavDown() {
     if (window.matchMedia("(min-width: 992px)").matches) {
-      navbarSquraeSub.classList.add("expanded");
-      navbarSquraeSub.classList.remove("shrink");
+      navbarSquareSubs.forEach(function(element) {
+        element.classList.remove("shrink");
+        element.classList.add("expanded");
+      });
     }
-
   }
   function navSubUpColorChange() {
 
@@ -106,3 +109,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
   }
 });
+
+// 스크롤시 함수
+var header = document.querySelector("#header");
+var mainheader = document.querySelector("#main-header");
+
+if(mainheader !== null) {
+  init();
+}
+
+function init(){  
+  window.onscroll = function() {scrollFunction()};
+  header.classList.add("d-lg-none");
+  header.classList.add("transition-effect");
+  mainheader.classList.add("transition-effect");
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    header.classList.remove("d-lg-none");
+    mainheader.classList.add("d-lg-none");
+  } else {
+    header.classList.add("d-lg-none");
+    mainheader.classList.remove("d-lg-none");
+  }
+}
+
